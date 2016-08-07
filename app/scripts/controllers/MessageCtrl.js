da11254125e4463be4a557a14360bbf26cafeee1
jmanock <jonmanock@gmail.com>
2016-08-07 08:30:28 -0400

    adding more scripts

---
+++ app/scripts/controllers/MessageCtrl.js
@@ -0,0 +1,20 @@
+(function(){
+  function MessageCtrl($scope, MessageSrv, $cookies){
+    $scope.send = function(){
+      if($scope.messageContent !== ''){
+        MessageSrv.addMessage(
+          {
+            content:$scope.messageContent,
+            roomID:$scope.selectedRoom.$id,
+            setAt:'default',
+            username:$cookies.get('currentUser')
+          }
+        );
+        $scope.messageContent = '';
+      }
+    };
+  }
+  angular
+  .module('chatterBox')
+  .controller('MessageCtrl', ['$scope', 'MessageSrv', '$cookies', MessageCtrl]);
+})();
---
+++ app/scripts/controllers/ModalInstanceCtrl.js
@@ -0,0 +1,14 @@
+(function(){
+  function ModalInstanceCtrl($scope, $uibModalInstance){
+    $scope.roomName = '';
+    $scope.submit = function(){
+      $uibModalInstance.close($scope.roomName);
+    };
+    $scope.close = function(){
+      $uibModalInstance.dismiss('cancel');
+    };
+  }
+  angular
+  .module('chatterBox')
+  .controller('ModalInstanceCtrl', ['$scope', '$uibModalInstance', ModalInstanceCtrl]);
+})();
