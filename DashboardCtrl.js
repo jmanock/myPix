5c09fc2a43570bf44bc66f44ec82b4970e11471c
jmanock <jonmanock@gmail.com>
2016-08-07 10:54:46 -0400

    dnfw

---
+++ DashboardCtrl.js
@@ -0,0 +1,14 @@
+var app = angular.module('firebaseLogin');
+app.controller('DashboardCtrl', function($scope,userReference, thingsReference){
+  $scope.profile = userReference;
+  $scope.things = thingsReferecnce;
+  $scope.addThing = function(){
+    $scope.things.$add($scope.thing);
+  };
+  $scope.removeThing = function(thing){
+    $scope.things.$remove(thing);
+  };
+  $scope.update = function(){
+    $scope.profile.$save();
+  };
+});
