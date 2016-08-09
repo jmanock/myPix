0202a189a7a8c908c6815565970b851777eb976e
jmanock <jonmanock@gmail.com>
2016-08-09 00:18:41 -0400

    breaking it down

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
     $('button').click(function(){
       var kewl = $('.text').val();
       $.getJSON(somethingKewl+kewl).success(function(data){
-        console.log(data);
+        console.log(data.results.length);
       });
     });
 
