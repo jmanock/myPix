fa022c5761e60b74337371acc306e930e570aafb
jmanock <jonmanock@gmail.com>
2016-08-25 17:28:46 -0400

    part 1.5 dont

--- scrap.js
+++ scrap.js
@@ -5,19 +5,32 @@ var sexting = [];
 request(url, function(error, response, body){
   if(!error && response.statusCode === 200){
     var $ = cheerio.load(body);
-    var imglink = $('a.thumbnail.may-blank.outbound');
+    var imglink = $('a.thumbnail.may-blank');
+
     //var title = $('.title.may-blank.outbound');
     var title = $('.title .may-blank');
 
-    imglink.each(function(i, link){
-      var url = $(link).attr('href');
-      //console.log(i, url);
-    });
-    title.each(function(i,link){
-      var something = $(link).text();
-      console.log(i,something);
-    });
+    for(var i = 0; i<imglink.length && i<title.length; i++){
+      var something = $(title[i]).text();
+      var url = $(imglink[i]).attr('href');
+      console.log(i, something, url);
+    }
+
+    // for(var j = 0; j<title.length; j++){
+    //   var something = $(title[j]).text();
+    //   //console.log(j, something);
+    // }
+    // imglink.each(function(i, link){
+    //   var url = $(link).attr('href');
+    //   //var something = $(link).text();
+    //   //console.log(i, something);
+    // });
+    // title.each(function(i,link){
+    //   var something = $(link).text();
+    //   // sexting.push(something);
+    // });
   }else{
     console.log('We fucked up or you fucked up');
   }
+  //console.log(sexting);
 });
