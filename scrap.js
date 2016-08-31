68e639b89f5b8270d51841452fe16f9d468b607e
jmanock <jonmanock@gmail.com>
2016-08-31 04:46:28 -0400

    redo page one

--- scrap.js
+++ scrap.js
@@ -1,6 +1,3 @@
-/* TODO
-  *
-*/
 var request = require('request');
 var cheerio = require('cheerio');
 var prompt = require('prompt');
@@ -10,54 +7,43 @@ var addons = '/by_name/index_pages/';
 prompt.start();
 
 prompt.get(['LastName','FirstName'], function(err, result){
-
   var lastName = result.LastName.toUpperCase();
   var firstName = result.FirstName.toUpperCase();
   var fullName = lastName + ', '+firstName;
   var firstLetter = result.LastName.charAt(0);
-  var linkFl = url+addons+firstLetter+'.html';
-console.log(fullName);
-  request(linkFl, function(error, response, body){
-    if(!error && response.statusCode === 200){
+  var linkFl = url + addons + firstLetter+'.html';
 
-        var $ = cheerio.load(body);
-        var secondLetterLookup = $('td font a');
-        var knew = [];
-        var other = [];
-        knew.push(lastName);
-        for(var i = 0; i<secondLetterLookup.length; i++){
-          var href = $(secondLetterLookup[i]).attr('href');
-          var lastNameText = $(secondLetterLookup[i]).text();
-          lastNameText = lastNameText.replace(/\r?\n|\r/g,"");
-          knew.push(lastNameText);
-          other.push(href);
-          knew.sort();
-        }
-        nextPage(knew, other);
+  request(linkFl, function(err, response, body){
+    if(!err && response.statusCode === 200){
+      doWork(body);
     }
-    var a;
-    function nextPage(knew, href){
-      for(var i = 0; i<knew.length; i++){
-        if(knew[i] === lastName){
-          a = knew.indexOf(lastName);
-          a = href[a-1];
-        }
-      }
-    }
-    request(a, function(error, response, body){
-      if(!error && response.statusCode === 200){
-        var $ = cheerio.load(body);
-        var something = $('td font a');
-        var mix = [];
-        for(var i = 0; i<something.length; i++){
-          var mine = $(something[i]).text();
-          mine = mine.replace(/\r?\n|\r/g,"");
-          mix.push(mine);
-
-        }
-
-      }
-    });
   });
+  function doWork(body){
+    var $ = cheerio.load(body);
+    var namesLookUp = $('td font a');
+    var namesArray = [];
+    var hrefsArray = [];
 
+    namesArray.push(lastName);
+
+    for(var i = 0; i<namesLookUp.length; i++){
+      var hrefs = $(namesLookUp[i]).attr('href');
+      var lastNamesList = $(namesLookUp[i]).text();
+      lastNamesList = lastNamesList.replace(/\r?\n|\r/g,"");
+      namesArray.push(lastNamesList);
+      hrefsArray.push(hrefs);
+      namesArray.sort();
+    }
+    nextPage(namesArray,hrefsArray);
+  }
+  var a;
+  function nextPage(namesArray,hrefsArray){
+    for(var i = 0; i<namesArray.length; i++){
+      if(namesArray[i] === lastName){
+        a = namesArray.indexOf(lastName);
+        a = hrefsArray[a-1];
+      }
+    }
+    console.log(a);
+  }
 });
